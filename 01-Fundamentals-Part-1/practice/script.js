245162b5e9c96a836de83464893b0560fbeb650e
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-20 03:55:33 +0600

    fundamental learning continue

---
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -0,0 +1,29 @@
+let js = "Amazing";
+// if (js == "Amazing") alert("javascript is fun");
+console.log(40 + 8 + 10 - 23);
+console.log("ratul");
+console.log(23);
+let firstName = "Bob";
+console.log(firstName);
+
+/* Good Variable Declaration Convention */
+let myFirstJob = "programmer";
+let myCurrentJob = "teacher";
+console.log(myFirstJob);
+
+/* Bad Variable Declaration Convention */
+let job1 = "programmer";
+let job2 = "teacher";
+/* 
+Assignment Values and Variables
+1.Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+2. Log their values to the console. 
+*/
+let country = "Bangladesh";
+let continent = "Asia";
+let population = "17 Billion";
+
+console.log(country);
+console.log(continent);
+console.log(population);
