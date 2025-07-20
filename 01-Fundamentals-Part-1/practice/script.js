f9bb843a192ac66c27f21a8b2155eb6790d2ffa4
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-20 18:16:57 +0600

    fundamental learning variable, operator

--- 01-Fundamentals-Part-1/practice/script.js
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -3,8 +3,8 @@ let js = "Amazing";
 console.log(40 + 8 + 10 - 23);
 console.log("ratul");
 console.log(23);
-let firstName = "Bob";
-console.log(firstName);
+//let firstName = "Bob";
+//console.log(firstName);
 
 /* Good Variable Declaration Convention */
 let myFirstJob = "programmer";
@@ -27,3 +27,37 @@ let population = "17 Billion";
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let javascriptIsFun = true; // boolean data type
+console.log(javascriptIsFun);
+console.log(typeof true); // boolean
+console.log(typeof 23); //number
+console.log(typeof "javascriptIsFun"); //string
+
+let year;
+console.log(typeof year); // undefined data type
+year = 2025;
+console.log(typeof year); // define a value to a undefined variable
+console.log(typeof null); // it's a bug
+
+//let // it's mutable declaration
+//const //it's an immutable declaration
+
+/* -------Operator--------*/
+/* -------Minus Operator--------*/
+const agaMinus = 2025 - 1985;
+console.log(agaMinus);
+/* -------PLus Operator--------*/
+const agePlus = 1985 + 40;
+console.log(agePlus);
+
+console.log(agaMinus * 2, agePlus / 2);
+
+/* -------Exponential Operator--------*/
+console.log(2 ** 3); // 2 to the power of 3, that means 2*2*2
+
+/* -------Concat Variable--------*/
+const firstName = "Muhtasim";
+const middleName = "Billah";
+const lastName = "Ratul";
+console.log(firstName + " " + middleName + " " + lastName);
