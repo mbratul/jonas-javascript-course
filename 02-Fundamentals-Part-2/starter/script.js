957b54571bae42fc8fc5326d9f68fd14934975cb
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-25 03:42:36 +0600

    function in javascript

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// function
+/* Functions in JavaScript are reusable blocks of code designed to perform specific tasks. They allow you to organize, reuse, and modularize code. It can take inputs, perform actions, and return outputs. */
+/* Key Characteristics of Functions
+- Parameters vs Arguments: Parameters are placeholders for function and arguments are actual value for those placeholders.
+- Return Values: Functions can return a value using the return keyword.
+- Default Parameters: Default values can be assigned to function parameters. */
+
+function logger() {
+  console.log("My Name is Jonas");
+}
+// invoking / calling / running -- a function
+/* Function Invocation
+The function code you have written will be executed whenever it is called.
+
+- Triggered by an event (e.g., a button click by a user).
+- When explicitly called from JavaScript code.
+- Automatically executed, such as in self-invoking functions. */
+logger();
+/* Function Parameters
+Parameters are input passed to a function. In the above example, sum(x , y) takes two parameters, x and y. */
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+// Argument : In JavaScript, an argument is the value that you pass to a function when you call it.
+fruitProcessor(5, 10);
+console.log(fruitProcessor(5, 10));
+
+const fruitJuice = fruitProcessor(2, 4); // store the value of a function
+console.log(fruitJuice);
