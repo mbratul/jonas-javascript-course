2b051b2ad3754a9cd529cacf1b53ca8fa42088ec
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-26 22:17:24 +0600

    object coding challenge complete

--- 02-Fundamentals-Part-2/starter/coding_challenge.js
+++ 02-Fundamentals-Part-2/starter/coding_challenge.js
@@ -71,3 +71,52 @@ const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(
   `bills are ${bills}, tips are ${tips}, total bills and tips are ${totals}`
 );
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
+*/
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBmi: function () {
+    return (this.bmi = this.mass / (this.height * this.height));
+  },
+};
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBmi: function () {
+    return (this.bmi = this.mass / (this.height * this.height));
+  },
+};
+console.log(mark.bmi);
+console.log(john.calcBmi());
+if (mark.calcBmi() >= john.calcBmi()) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.calcBmi()}) is higher than ${
+      john.fullName
+    }'s (${john.calcBmi()})!`
+  );
+} else if (mark.calcBmi() <= john.calcBmi()) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.calcBmi()}) is lower than ${
+      john.fullName
+    }'s (${john.calcBmi()})!`
+  );
+} else {
+  console.log("Both BMI are same");
+}
