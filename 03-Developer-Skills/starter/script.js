599235428405d514d90af37551313034ae7f0a7b
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-29 01:57:43 +0600

    coding challeng completed

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+///////////////////////////////////////
+// Debugging with the Console and Breakpoints
+const measurekelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    //value: Number(prompt("Degree Celsius:")),
+    value: 10,
+  };
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measurekelvin());
