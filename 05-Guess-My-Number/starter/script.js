c878e5cc213000e26a79a4d8b2997e2485e1028b
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-30 20:23:56 +0600

    learning DOM continue

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,9 +9,9 @@ console.log((document.querySelector(".score").textContent = 10));
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value); */
 //secret number
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
+//document.querySelector(".number").textContent = secretNumber;
 
 //message element variable
 const messageElem = document.querySelector(".message");
@@ -20,11 +20,17 @@ const checkBtnElem = document.querySelector(".check");
 checkBtnElem.addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
+  // When there is no input
   if (!guess) {
     console.log((messageElem.textContent = "No Number"));
-  } else if (guess === secretNumber) {
+  } // when player wins
+  else if (guess === secretNumber) {
     messageElem.textContent = "Correct Number";
-  } else if (guess > secretNumber) {
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+  } //when gues is too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       messageElem.textContent = "Too High";
       score--;
@@ -33,7 +39,8 @@ checkBtnElem.addEventListener("click", function () {
       messageElem.textContent = "You Lost the Game";
       document.querySelector(".score").textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  } // when guess is too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       messageElem.textContent = "Too Low";
       score--;
@@ -43,4 +50,30 @@ checkBtnElem.addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  Math.trunc(Math.random() * 20) + 1;
+  messageElem.textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
