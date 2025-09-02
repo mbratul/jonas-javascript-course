14c4eaa2fe5843c6da4b64e01046ea329cd7a662
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-09-02 21:29:42 +0600

    learning date operation in js

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -81,20 +81,40 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const combineMovsDates = acc.movements.map(function (mov, i) {
+    return { movement: mov, movementDate: acc.movementsDates.at(i) };
+  });
+  //console.log(combineMovsDates);
+  if (sort)
+    combineMovsDates.sort(function (a, b) {
+      return a.movement - b.movement;
+    });
+  /* const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements; */
 
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? "deposit" : "withdrawal";
+  combineMovsDates.forEach(function (obj, i) {
+    const { movement, movementDate } = obj;
+    const type = movement > 0 ? "deposit" : "withdrawal";
+
+    const date = new Date(movementDate);
+
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    const displayDate = `${day}/${month}/${year}`;
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov.toFixed(2)}€</div>
+        <div class="movements__date">${displayDate}</div>
+        <div class="movements__value">${movement.toFixed(2)}€</div>
       </div>
     `;
 
@@ -142,7 +162,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -154,6 +174,13 @@ const updateUI = function (acc) {
 ///////////////////////////////////////
 // Event handlers
 let currentAccount;
+
+//Fake Always Logged IN
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
+//day/month/year
 
 btnLogin.addEventListener("click", function (e) {
   // Prevent form from submitting
@@ -170,6 +197,15 @@ btnLogin.addEventListener("click", function (e) {
       currentAccount.owner.split(" ")[0]
     }`;
     containerApp.style.opacity = 100;
+
+    //Create current Date and Time
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hours = `${now.getHours()}`.padStart(2, 0);
+    const minutes = `${now.getMinutes()}`.padStart(2, 0);
+    labelDate.textContent = `${day}/${month}/${year}, ${hours}:${minutes}`;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = "";
@@ -198,6 +234,10 @@ btnTransfer.addEventListener("click", function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    //Add Transfer Date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -214,6 +254,9 @@ btnLoan.addEventListener("click", function (e) {
   ) {
     // Add movement
     currentAccount.movements.push(amount);
+
+    //Add Loan Date
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -247,7 +290,7 @@ btnClose.addEventListener("click", function (e) {
 let sorted = false;
 btnSort.addEventListener("click", function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
 
@@ -424,4 +467,36 @@ btnSort.addEventListener("click", function (e) {
   //Divisions
   console.log(10n / 3n);
   console.log(10 / 3);
+}
+{
+  /////////////////////////////////////////////////
+  //create a date
+  console.log("---create a date-----");
+
+  /* const currentDate = new Date();
+  console.log(currentDate);
+  console.log(new Date("Sep 02 2025 14:23:00"));
+  console.log(new Date("December 24, 2024"));
+  console.log(new Date(account1.movementsDates[0]));
+  console.log(new Date(2034, 10, 25, 4, 15, 26));
+  console.log(new Date(0));
+  console.log(new Date(3 * 24 * 60 * 60 * 1000)); */
+
+  //working with Dates
+  const future = new Date(2034, 10, 25, 4, 15);
+  console.log(future.getFullYear());
+  console.log(future.getMonth());
+  console.log(future.getDate());
+  console.log(future.getDay());
+  console.log(future.getHours());
+  console.log(future.getMinutes());
+  console.log(future.getSeconds());
+  console.log(future.toISOString());
+  console.log(future.getTime());
+
+  console.log(new Date(2048019300000));
+  console.log(Date.now());
+
+  future.setFullYear(2035);
+  console.log(future);
 }
