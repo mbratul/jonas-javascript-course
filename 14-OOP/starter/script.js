48a7c9326d12e77207f5a11b1cb96076cfabd9bb
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-09-09 05:40:30 +0600

    learning constructor function in js and the new keyword

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+function Person(firstName, birthYear) {
+  //Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //use method
+  //it's not a good practice to write method inside a constructor function
+  /* const calcAge = function () {
+    console.log(2025 - this.birthYear);
+  }; */
+}
+const ratul = new Person("ratul", 1985);
+const mamun = new Person("mamun", 1995);
+const halim = new Person("halim", 1975);
+console.log(ratul, mamun, halim);
+console.log(Person.calcAge(ratul));
+const jack = "jack";
+console.log(ratul instanceof Person);
+console.log(jack instanceof Person);
+
+// 1. New{} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
