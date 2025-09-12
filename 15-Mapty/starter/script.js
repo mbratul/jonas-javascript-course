9a98d592d70080bfb7b3f31936f2bf09eda22544
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-09-13 03:50:02 +0600

    learning start chapter 15 mapty app development

---
+++ 15-Mapty/starter/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+//Geolocation API
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `My current location is ${latitude} Latitude, and ${longitude} Longitude`
+      );
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const cords = [latitude, longitude];
+      console.log(cords);
+
+      //leaflet JS library code start
+      const map = L.map("map").setView(cords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(cords)
+        .addTo(map)
+        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+        .openPopup();
+      //leaflet JS library code end
+    },
+    function () {
+      console.log("Could not found your location");
+    }
+  );
+}
