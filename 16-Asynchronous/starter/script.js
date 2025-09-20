cfefa86f54a81e808ef3c38fda767bf353224bf5
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-09-20 11:43:29 +0600

    learning promise in js

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -146,3 +146,62 @@ const countriesContainer = document.querySelector(".countries");
     }, 20000);
   }, 1000); */
 }
+{
+  ///////////////////////////////////////
+  //fetch api call
+  // const request = new XMLHttpRequest();
+  //   request.open(
+  //     "GET",
+  //     //`https://restcountries.com/v3.1/name/${country}?fullText=true`
+  //     `https://restcountries.com/v3.1/name/${country}?fullText=true`
+  //   );
+  //   request.send();
+  console.log("----fetch api call---");
+  const renderCountry = function (data, className = "") {
+    const html = `
+        <article class="country ${className}">
+        <img class="country__img" src="${data.flags.png}" alt="${
+      data.flags.alt
+    }" />
+            <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              Number(data.population) / 10000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(
+              data.languages
+            )}</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+                </div>
+                </article>
+                `;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  };
+  const request = fetch("https://restcountries.com/v3.1/name/bangladesh");
+  console.log(request);
+
+  const getCountryData = function (country) {
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+      .then(function (response) {
+        //console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        renderCountry(data[0]);
+      });
+  };
+  getCountryData("india");
+
+  //simplified arrow function version
+  const getCountryDataArrow = function (country) {
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+      .then((response) => response.json())
+      .then((data) => renderCountry(data[0]));
+  };
+  getCountryDataArrow("pakistan");
+}
